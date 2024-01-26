209bc5fd935a336b7211983328832b7ac3f0df0d
nickhealthy <alskadmlcraz1@gmail.com>
2024-01-26 15:14:29 +0900

    6. 스프링 MVC 기본 기능 - HttpMessageConverter, ArgumentResolver, ReturnValue

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -12,6 +12,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * [요약]
+ * @ResponseBody
+ * - HTTP BODY에 문자 내용을 직접 반환
+ * - viewResolver 대신 HttpMessageConverter(canRead - read, canWrite - write 메서드 사용)가 동작
+ *   - 응답의 경우 클라이언트의 Accept 헤더와 서버의 컨트롤러 반환 타입 정보 둘을 조합해서 HttpMessageConverter가 선택됨
+ *
+ * [HTTP 요청 데이터 읽기 프로세스]
+ * 1. HTTP 요청이 오고, 컨트롤러에서 @RequestBody, HttpEntity 파라미터를 사용
+ * 2. 메시지 컨버터가 메시지를 읽을 수 있는지 확인하기 위해 canRead()를 호출
+ * - 대상 클래스 타입을 지원하는지,
+ * - HTTP 요청의 Content-type 미디어 타입을 지원하는지
+ * 3. canRead() 조건 만족 시 read()를 호출해 객체를 생성해서 반환
+ *
+ * [HTTP 응답 데이터 생성 프로세스]
+ * 1. 컨트롤럴에서 @ResponseBody, HttpEntity로 값을 반환
+ * 2. 메시지 컨버터가 메시지를 쓸 수 있는지 확인하기 위해 canWrite() 를 호출
+ * - 대상 클래스 타입을 지원하는지,
+ * - HTTP 요청의 Accept 미디어 타입을 지원하느지(더 정확히는 @RequestMapping의 produces)
+ * 3. canWrite() 조건을 만족하면 write()를 호출해서 HTTP 응답 메시지 바디에 데이터를 생성
+ */
+
 @Slf4j
 @Controller
 //@RestController
